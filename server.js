832aebb09c2243d797f0d1eaf62932feb5bc7a10
iankerby <pablo.iankerby@gmail.com>
2025-05-05 09:49:17 +0800

    Fix Heroku deployment

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve static files
 app.use(express.static(path.join(__dirname, 'web-build')));
 
+// Handle all routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'web-build', 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
