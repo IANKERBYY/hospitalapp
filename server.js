b5d72da88d755d5a178fed8c513306fbb10be59c
iankerby <pablo.iankerby@gmail.com>
2025-05-05 09:42:38 +0800

    Setup Heroku deployment

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from React Native web build
 app.use(express.static(path.join(__dirname, 'web-build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'web-build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
